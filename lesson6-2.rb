ef4faebd0deac449bb133f4f4d50cad840532936
EC2 Default User <ec2-user@ip-172-31-1-76.ap-northeast-1.compute.internal>
2023-06-08 08:32:53 +0000

    initial commit1

---
+++ lesson6-2.rb
@@ -0,0 +1,48 @@
+puts "計算を始めます
+何回繰り返しますか？"
+i = gets.to_i
+for n in 1..i do
+  puts "#{n}回目の計算"
+  puts "2つの値を入力してください"
+
+  a = gets.to_i
+  b = gets.to_i
+
+  puts "a=#{a}"
+  puts "b=#{b}"
+
+  puts "計算結果を出力します"
+  puts "#{a}+#{b}=#{a + b}"
+  puts "#{a}-#{b}=#{a - b}"
+  puts "#{a}*#{b}=#{a * b}"
+  puts "#{a}/#{b}=#{a / b}"
+end
+puts "計算を終了します"
+
+#別解
+#puts "計算をはじめます"
+#puts "何回繰り返しますか？"
+
+#input = gets.to_i
+
+#i = 1
+#while i <= input do
+#  puts "#{i}回目の計算"
+#  puts "2つの値を入力してください"
+
+#  a = gets.to_i
+#  b = gets.to_i
+
+#  puts "a=#{a}"
+#  puts "b=#{b}"
+
+#  puts "計算結果を出力します"
+#  puts "#{a}+#{b}=#{a + b}"
+#  puts "#{a}-#{b}=#{a - b}"
+#  puts "#{a}*#{b}=#{a * b}"
+#  puts "#{a}/#{b}=#{a / b}"
+
+#  i += 1
+#end
+
+#puts "計算を終了します"
